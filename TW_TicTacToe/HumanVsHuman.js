75cc213873332243aa59d5a851f22abcc1f51962
Katharina Washüttl <coding.katharina@gmail.com>
2022-09-28 14:48:27 +0200

    deleted uneccesary codes & formatted codes correctly

--- TW_TicTacToe/HumanVsHuman.js
+++ TW_TicTacToe/HumanVsHuman.js
@@ -1,12 +1,11 @@
 //TODOs
-//Doppelte Eingaben Loop
-//Tie Condition
-//Aus While (GameIsRunning) Loop ausbrechen
+//doppelte Eingaben Loop
+//tie Condition (if both players didn't win) - integrated counter?
+//other play modes 
 
 
 const prompt = require('prompt-sync')();
 
-//********************* Variables **************************
 //*********************** Texts ****************************
 
 const INTRO_TEXT = "What Mode would you like to play?";
@@ -16,16 +15,17 @@ const GAME_MODE_TEXT = "You chose: ";
 const MOVE_INSTRUCTION = "Please enter a move by typing a possible position (Like: a1)."
 const WRONG_ENTRY = "Position already taken or non existent."
 
-let quittingOut = "QUIT";
+//********************* Variables **************************
 
-let countMoves = 0; 
+const PLAYER1 = " x";
+const PLAYER2 = " o";
 
-let Player1 = " x";
-let Player2 = " o";
-
-//Exit
+//Quit/Exit game completely
+const QUITTING_OUT = "QUIT";
 let process = require ('process');
 let gameIsRunning = true;
+
+let countMoves = 0; //TODO - yet to be integrated
 
 let grid = [["a1","a2","a3"], 
             ["b1","b2","b3"], 
@@ -33,10 +33,10 @@ let grid = [["a1","a2","a3"],
 
 /*
 let GRID2 = " a1 | a2 | a3  \n"+
-                "----|----|---- \n"+
-               " b1 | b2 | b3  \n"+
-               "----|----|---- \n"+
-               " c1 | c2 | c3  \n";
+            "----|----|---- \n"+
+            " b1 | b2 | b3  \n"+
+            "----|----|---- \n"+
+            " c1 | c2 | c3  \n";
 */
 
 
@@ -57,210 +57,150 @@ function displayGRID() {
     console.log(grid[2]);             
 }
 
-//for picking game mode & potential quitting
-function pickMode() {
-    let 
-}
-
 //User gives moving input (e.g:B2,C1,...) & input is logged into Array
 function getUserInput() {
     let userMove = prompt(displayText(MOVE_INSTRUCTION)).toUpperCase();     
     userMovesMade.push(userMove);
     }                                              
 
-//Quit on demand
-
-//WinningConditions
-function winningCondition (Player1,Player2) {
+//Winning conditions
+function winningCondition (PLAYER1,PLAYER2) {
         //Winning conditions for Player 1
-    if (grid[0][0] === Player1 && grid[0][1] === Player1 && grid[0][2] === Player1 || 
-        grid[1][0] === Player1 && grid[1][1] === Player1 && grid[1][2] === Player1 ||
-        grid[2][0] === Player1 && grid[2][1] === Player1 && grid[2][2] === Player1 ||
-        grid[0][0] === Player1 && grid[1][0] === Player1 && grid[2][0] === Player1 ||
-        grid[0][1] === Player1 && grid[1][1] === Player1 && grid[2][1] === Player1 ||
-        grid[0][2] === Player1 && grid[1][2] === Player1 && grid[2][2] === Player1 ||
-        grid[0][0] === Player1 && grid[1][1] === Player1 && grid[2][2] === Player1 ||
-        grid[0][2] === Player1 && grid[1][1] === Player1 && grid[2][0] === Player1)
+    if (grid[0][0] === PLAYER1 && grid[0][1] === PLAYER1 && grid[0][2] === PLAYER1 || 
+        grid[1][0] === PLAYER1 && grid[1][1] === PLAYER1 && grid[1][2] === PLAYER1 ||
+        grid[2][0] === PLAYER1 && grid[2][1] === PLAYER1 && grid[2][2] === PLAYER1 ||
+        grid[0][0] === PLAYER1 && grid[1][0] === PLAYER1 && grid[2][0] === PLAYER1 ||
+        grid[0][1] === PLAYER1 && grid[1][1] === PLAYER1 && grid[2][1] === PLAYER1 ||
+        grid[0][2] === PLAYER1 && grid[1][2] === PLAYER1 && grid[2][2] === PLAYER1 ||
+        grid[0][0] === PLAYER1 && grid[1][1] === PLAYER1 && grid[2][2] === PLAYER1 ||
+        grid[0][2] === PLAYER1 && grid[1][1] === PLAYER1 && grid[2][0] === PLAYER1)
         {
             console.log("Player 1 won the game!");
-            process.exit(gameIsRunning); //TODO: break from function?
+            process.exit(gameIsRunning); //breaks from function if player won
 
     }
-    if (grid[0][0] === Player2 && grid[0][1] === Player2 && grid[0][2] === Player2 || 
-        grid[1][0] === Player2 && grid[1][1] === Player2 && grid[1][2] === Player2 ||
-        grid[2][0] === Player2 && grid[2][1] === Player2 && grid[2][2] === Player2 ||
-        grid[0][0] === Player2 && grid[1][0] === Player2 && grid[2][0] === Player2 ||
-        grid[0][1] === Player2 && grid[1][1] === Player2 && grid[2][1] === Player2 ||
-        grid[0][2] === Player2 && grid[1][2] === Player2 && grid[2][2] === Player2 ||
-        grid[0][0] === Player2 && grid[1][1] === Player2 && grid[2][2] === Player2 ||
-        grid[0][2] === Player2 && grid[1][1] === Player2 && grid[2][0] === Player2)
+    if (grid[0][0] === PLAYER2 && grid[0][1] === PLAYER2 && grid[0][2] === PLAYER2 || 
+        grid[1][0] === PLAYER2 && grid[1][1] === PLAYER2 && grid[1][2] === PLAYER2 ||
+        grid[2][0] === PLAYER2 && grid[2][1] === PLAYER2 && grid[2][2] === PLAYER2 ||
+        grid[0][0] === PLAYER2 && grid[1][0] === PLAYER2 && grid[2][0] === PLAYER2 ||
+        grid[0][1] === PLAYER2 && grid[1][1] === PLAYER2 && grid[2][1] === PLAYER2 ||
+        grid[0][2] === PLAYER2 && grid[1][2] === PLAYER2 && grid[2][2] === PLAYER2 ||
+        grid[0][0] === PLAYER2 && grid[1][1] === PLAYER2 && grid[2][2] === PLAYER2 ||
+        grid[0][2] === PLAYER2 && grid[1][1] === PLAYER2 && grid[2][0] === PLAYER2)
         {
             console.log("Player 2 won the game!");
-            process.exit(gameIsRunning); //TODO: break from function?
+            process.exit(gameIsRunning); //breaks from function if player won
         }
         }
 
-//place user input 
-function checkUserInput (input,Player) {
-    if (input === quittingOut) {
+//Place user input 
+function checkUserInput (input,Player) 
+{
+    if (input === QUITTING_OUT) {     //wenn quit eingegeben wird, stoppt das spiel
         console.log(":(");
         console.log("Thanks for playing.");
         process.exit();
-    }                  
-                                //add placeholder
-    else if (input === "A1") {
-            grid[0].splice(0,1,Player);  
-            displayGRID();                          //an Stelle A1 wird mit splice ein X eingesetzt                                           //neues (untereinander) Grid wird durch function angezeigt
+    }                                                
+        else if (input === "A1") {
+                grid[0].splice(0,1,Player);   //an Stelle A1 wird mit splice ein X eingesetzt
+                displayGRID();                //neues board wird angezeigt
             }                                    
-    else if (input === "A2") {
-            grid[0].splice(1,1,Player);
-            //console.log(GRID2.replace("a2",Player));
-            displayGRID();
+        else if (input === "A2") {
+                grid[0].splice(1,1,Player);
+                displayGRID();
             }
-    else if (input === "A3") {
-            grid[0].splice(2,1,Player);
-            //console.log(GRID2.replace("a3",Player));
-            displayGRID();
+        else if (input === "A3") {
+                grid[0].splice(2,1,Player);
+                displayGRID();
             }
-    else if (input === "B1") {
-            //GRID2.replace("b1",Player);
-            grid[1].splice(0,1,Player);
-            displayGRID();
+        else if (input === "B1") {
+                grid[1].splice(0,1,Player);
+                displayGRID();
             }
-    else if (input === "B2") {
-            //console.log(GRID2.replace("b2",Player));
-            grid[1].splice(1,1,Player);
-            displayGRID();
+        else if (input === "B2") {
+                grid[1].splice(1,1,Player);
+                displayGRID();
             }
-    else if (input === "B3") {
-            //console.log(GRID2.replace("b3",Player));
-            grid[1].splice(2,1,Player);
-            displayGRID();
+        else if (input === "B3") {
+                grid[1].splice(2,1,Player);
+                displayGRID();
             }
-    else if (input === "C1") {
-            //console.log(GRID2.replace("c1",Player));
-            grid[2].splice(0,1,Player);
-            displayGRID();
+        else if (input === "C1") {
+                grid[2].splice(0,1,Player);
+                displayGRID();
             }
-    else if (input === "C2") {
-            //console.log(GRID2.replace("c2",Player));
-            grid[2].splice(1,1,Player);
-            displayGRID();
+        else if (input === "C2") {
+                grid[2].splice(1,1,Player);
+                displayGRID();
             }
-    else if (input === "C3") {
-            //console.log(GRID2.replace("c3",Player));
-            grid[2].splice(2,1,Player);
-            displayGRID();
+        else if (input === "C3") {
+                grid[2].splice(2,1,Player);
+                displayGRID();
             }
-    else  {displayText(WRONG_ENTRY);
-            displayGRID();}
+                else  {displayText(WRONG_ENTRY);
+                        displayGRID();}
 
 }
-
-/*
-function checkForDoubles () {
-    if (grid.includes(Player1)) {
-        console.log("Doppelte Eingabe");
-    }
-}
-*/
-
-/*
-function checkBoard (Player) {
-    if (userMovesMade.length > 1) {                    //wenn bereits ein move gemacht wurde
-    for (i=0; i<= userMovesMade.length; i++) {         //so viele moves wie gemacht wurden, werden im loop ausgefüllt
-        GRID2.replace(userMovesMade[2],Player)
-    }
-}
-}
-*/
-
-/*
-function checkPriorBoard (Player) {
-    if (userMovesMade.length > 1) {                    //wenn bereits ein move gemacht wurde
-        for (i=0; i<= userMovesMade.length; i++) { 
-            if (userMovesMade.includes("a1")) {
-                GRID2.replace("a1",Player);
-    }               else if (userMovesMade.includes("a2")){
-                        GRID2.replace("a2",Player);
-    }               else if (userMovesMade.includes("a3")) {
-                        GRID2.replace("a3",Player);
-    }               else if (userMovesMade.includes("b1")) {
-                        GRID2.replace("b1",Player);
-    }               else if (userMovesMade.includes("b2")) {
-                        GRID2.replace("b2",Player);
-    }               else if (userMovesMade.includes("b3")) {
-                        GRID2.replace("b3",Player);
-    }               else if (userMovesMade.includes("c1")) {
-                        GRID2.replace("c1",Player);
-    }               else if (userMovesMade.includes("c2")) {
-                        GRID2.replace("c2",Player);
-    }               else if (userMovesMade.includes("c3")) {
-                        GRID2.replace("c3",Player);
-}
-}
-    }
-}
-*/
 
 function HumanVsHuman () {
     displayText(`${GAME_MODE_TEXT}: ${GAME_MODES[0]}`);     //displaying text: You chose 1. Human vs Human
     while(gameIsRunning) {
-        displayGRID(); 
-        getUserInput();
-        console.clear();                                         //asks player1 for a move
-        checkUserInput(userMovesMade[0],Player1);                  
-        getUserInput();
+        displayGRID();                                     //shows board
+        getUserInput();                                    //player 1 makes move (a1,a2,...)
+        console.clear();                                   //clears console for visual clarity    
+        checkUserInput(userMovesMade[0],PLAYER1);          //places user move in array & displays updated board         
+        getUserInput();                                    //player 2 makes move (a1,a2,...)
         console.clear(); 
-        checkUserInput(userMovesMade[1],Player2);  
+        checkUserInput(userMovesMade[1],PLAYER2);  
         getUserInput();  
-        console.clear();                                     //asks player1 for a move
-        checkUserInput(userMovesMade[2],Player1);
+        console.clear();                                     
+        checkUserInput(userMovesMade[2],PLAYER1);
         getUserInput();
-        console.clear();                                                            //asks player1 for a move
-        checkUserInput(userMovesMade[3],Player2);                                                   //asks player1 for a move                
+        console.clear();                                                            
+        checkUserInput(userMovesMade[3],PLAYER2);                                                                
         getUserInput();
-        console.clear();                                        //asks player1 for a move
-        checkUserInput(userMovesMade[4],Player1);
-        winningCondition(Player1,Player2);                 
+        console.clear();                                      
+        checkUserInput(userMovesMade[4],PLAYER1);
+        winningCondition(PLAYER1,PLAYER2);                  //checks for winning conditions - placed here because first time a win would be possible            
         getUserInput();   
-        checkUserInput(userMovesMade[5],Player2);
-        winningCondition(Player1,Player2); 
+        checkUserInput(userMovesMade[5],PLAYER2);
+        winningCondition(PLAYER1,PLAYER2); 
         getUserInput();
-        console.clear();                                      //asks player1 for a move
-        checkUserInput(userMovesMade[6],Player1);
-        winningCondition(Player1,Player2);                 
-        getUserInput();                                         //asks player1 for a move
-        checkUserInput(userMovesMade[7],Player2);
-        winningCondition(Player1,Player2);                 
+        console.clear();                                      
+        checkUserInput(userMovesMade[6],PLAYER1);
+        winningCondition(PLAYER1,PLAYER2);                 
+        getUserInput();                                         
+        checkUserInput(userMovesMade[7],PLAYER2);
+        winningCondition(PLAYER1,PLAYER2);                 
         getUserInput();
-        console.clear();                                          //asks player1 for a move
-        checkUserInput(userMovesMade[8],Player1);
-        winningCondition(Player1,Player2);                 
-        getUserInput();                                         //asks player1 for a move
-        checkUserInput(userMovesMade[9],Player2); 
-        winningCondition(Player1,Player2);                                     
+        console.clear();                                         
+        checkUserInput(userMovesMade[8],PLAYER1);
+        winningCondition(PLAYER1,PLAYER2);                 
+        getUserInput();                                        
+        checkUserInput(userMovesMade[9],PLAYER2); 
+        winningCondition(PLAYER1,PLAYER2);                                     
     } console.log("Nobody won.");
 }
 
+//TODO: other play modes yet to be coded 
 function RandomVsRandom () {
-    displayText(`${GAME_MODE_TEXT}: ${GAME_MODES[1]}`);     //displaying text: You chose 2. Random AI vs Random AI
+    displayText(`${GAME_MODE_TEXT}: ${GAME_MODES[1]}`);     //displaying text: You chose: 2. Random AI vs Random AI
     displayGRID(); 
-    getUserInput();                                         //asks player for a move
+    getUserInput();                                         
     checkUserInput(userMovesMade[0]);
 }
 
 function HumanVsRandom () {
     displayText(`${GAME_MODE_TEXT}: ${GAME_MODES[2]}`);     //displaying text: You chose 3. Human vs Random AI
     displayGRID(); 
-    getUserInput();                                         //asks player for a move
+    getUserInput();                                        
     checkUserInput(userMovesMade[0]);
 }
 
 function HumanVsUnbeatable () {
     displayText(`${GAME_MODE_TEXT}: ${GAME_MODES[3]}`);     //displaying text: You chose 4. Human vs Unbeatable AI
     displayGRID(); 
-    getUserInput();                                         //asks player for a move
+    getUserInput();                                         
     checkUserInput(userMovesMade[0]);
 }
 
@@ -272,7 +212,7 @@ displayText(GAME_MODES);
 
 let playMode = prompt(displayText(CHOOSE_MODES));
 modePicked.push(playMode);
-    if (modePicked[0] === quittingOut) {
+    if (modePicked[0] === QUITTING_OUT) {
         console.log(":(");
         console.log("Thanks for playing.");
         process.exit();   
